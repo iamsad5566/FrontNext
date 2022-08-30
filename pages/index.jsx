565980ee400a9d401f260d7e146c9cda1891dc70
sam <sam-chen@eup.com.tw>
2022-08-30 13:02:11 +0800

    feat: hide information

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,7 +29,7 @@ const Home = () => {
     if (window.confirm("Are you sure to logout?")) {
       authenticationService.logout();
       alert("logged out!");
-      document.cookie = `${setting.admin}=${setting.password};expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
+      document.cookie = `${setting.admin}="";expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
       setLoginButton("Login if you are yk");
       setTimeout(() => {
         window.location.reload();
