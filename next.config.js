2a4fc6f2cc21b43807c684d4a6293e726e1cfb19
sam <sam-chen@eup.com.tw>
2022-08-30 09:28:40 +0800

    feat: fix next.config

--- next.config.js
+++ next.config.js
@@ -2,19 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-
-    return config;
-  },
 };
 
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  nextConfig,
-  assetPrefix: isProd ? "https://tw-yk.com" : undefined,
-};
+module.exports = nextConfig;
