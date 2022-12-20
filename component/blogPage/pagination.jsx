50e77147adf543e110c14b9d63feacaf9679a279
Yen-Kuang-tw <nf8964p5566@gmail.com>
2022-12-20 23:21:11 +0800

    feat: add page selector

--- component/blogPage/pagination.jsx
+++ component/blogPage/pagination.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import _ from "lodash";
 
 const Pagination = (props) => {
@@ -10,9 +10,15 @@ const Pagination = (props) => {
     handlePageMinus,
     handlePagePlus,
   } = props;
+
   const pagesCount = Math.ceil(numbersOfItems / pageSize);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
+  const [inputPage, setInputPage] = useState("");
+
+  function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+  }
 
   return (
     <div style={{ textAlign: "center" }}>
@@ -54,6 +60,45 @@ const Pagination = (props) => {
               <span aria-hidden="true">&raquo;</span>
             </button>
           </li>
+          <input
+            id="pageSelector"
+            className="page-link"
+            style={{
+              marginLeft: "1em",
+              width: "2em",
+              padding: "0em",
+              textAlign: "center",
+            }}
+            value={inputPage}
+            onChange={(event) => {
+              if (isNumeric(event.nativeEvent.data)) {
+                setInputPage(event.target.value);
+              } else if (event.nativeEvent.data === null) {
+                setInputPage(
+                  event.target.value.substring(0, event.target.value.length)
+                );
+              }
+            }}
+            onKeyDown={(event) => {
+              if (
+                event.code === "Enter" &&
+                isNumeric(inputPage) &&
+                inputPage <= pagesCount
+              ) {
+                handlePageChange(parseInt(inputPage, 10));
+              }
+
+              if (event.code === "Enter" && !isNumeric(inputPage)) {
+                alert("Please input a number!");
+                return;
+              }
+
+              if (event.code === "Enter" && inputPage > pagesCount) {
+                alert("Do not have so many pages!");
+                return;
+              }
+            }}
+          />
         </ul>
       </nav>
     </div>
